640b3ec204b0c353fb04e0ea6065e0f73ee3977f
MarcosAUlaTeste <marcos.vaz@ba.estudante.senai.br>
2025-06-11 20:17:59 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // ajuste conforme a porta do seu backend
+  baseURL: 'https://restaurante-backend-2svy.onrender.com/api/pratos', // ajuste conforme a porta do seu backend
 });
 
 export default api;
